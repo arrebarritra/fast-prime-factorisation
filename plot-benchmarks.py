192a805fbb854ac74655013a249899d059f17282
aritra <aritra@kth.se>
2022-10-14 18:13:55 +1100

    Add latex font to plots

--- plot-benchmarks.py
+++ plot-benchmarks.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from algorithms import ALGORITHMS
 
+plt.rc('text', usetex=True)
+
 bench = pd.read_csv('benchmark.csv')
 for alg in ALGORITHMS:
     x = bench[bench['algorithm']==alg]['N'].to_numpy()
@@ -10,5 +12,10 @@ for alg in ALGORITHMS:
         xint += [int(el)]
     y = bench[bench['algorithm']==alg]['time']
     plt.semilogx(xint, y, marker='o', linestyle='None', label=alg)
+
+plt.xlabel('$N$')
+plt.ylabel('Time [s]')
 plt.legend()
+
+
 plt.show()
