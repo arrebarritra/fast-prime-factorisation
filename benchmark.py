3d3bfd347b5b5d11c22686e5fb25e38e39e3b5a9
aritra <aritra@kth.se>
2022-10-14 17:37:29 +1100

    Run benchmarks for small test instances

--- benchmark.py
+++ benchmark.py
@@ -1,17 +1,21 @@
 from algorithms import ALGORITHMS
-import numpy as np
+import math
 import pandas as pd
 from time import time
+
 
 for alg in ALGORITHMS:
     semiprimes = open('semiprimes.txt')
     for N in semiprimes:
+        if len(N) > 15:
+            if alg == "Trial division":
+                continue
         N = int(N)
         start = time()
         if alg == "Quadratic sieve":
-            L = np.exp(np.sqrt(np.log(N)*np.log(np.log(N))))
-            B = int(np.power(L,np.sqrt(2)/4))
-            M = int(np.power(L,3*np.sqrt(2)/4))
+            L = math.exp(math.sqrt(math.log(N)*math.log(math.log(N))))
+            B = int(math.pow(L,math.sqrt(2)/4))
+            M = int(math.pow(L,3*math.sqrt(2)/4))
             ALGORITHMS[alg](N, B, M)
         else:
             ALGORITHMS[alg](N)
