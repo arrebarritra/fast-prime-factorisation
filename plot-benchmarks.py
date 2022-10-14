7a2802945d37513dffceb8d6b6bae7c956e9087f
aritra <aritra@kth.se>
2022-10-14 18:02:26 +1100

    Hacky fix for large integers N not working with pandas/numpy for plotting

--- plot-benchmarks.py
+++ plot-benchmarks.py
@@ -4,8 +4,11 @@ from algorithms import ALGORITHMS
 
 bench = pd.read_csv('benchmark.csv')
 for alg in ALGORITHMS:
-    x = bench[bench['algorithm']==alg]['N']
+    x = bench[bench['algorithm']==alg]['N'].to_numpy()
+    xint = []
+    for el in x:
+        xint += [int(el)]
     y = bench[bench['algorithm']==alg]['time']
-    plt.semilogx(x, y, label=alg)
+    plt.semilogx(xint, y, marker='o', linestyle='None', label=alg)
 plt.legend()
 plt.show()
