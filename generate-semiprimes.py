d9efdee72307551ce2e73953d62e1d1be194a7d0
aritra <aritra@kth.se>
2022-10-20 23:35:50 +1100

    Add final semiprime generation parameters and semiprimes

--- generate-semiprimes.py
+++ generate-semiprimes.py
@@ -1,11 +1,14 @@
 from sympy.ntheory.generate import randprime
 
-interval_boundaries = [10**i for i in [5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20]]
-amount_per_interval = 5
+interval_boundaries = [10**i for i in [5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 25, 27, 30, 33, 35]]
+amount_per_interval = 1
 
 file = open('semiprimes.txt', 'a')
 for i in range(len(interval_boundaries)-1):
     for j in range(amount_per_interval):
-        p1 = randprime(interval_boundaries[i], interval_boundaries[i+1])
-        p2 = randprime(interval_boundaries[i], interval_boundaries[i+1])
-        file.write(str(p1*p2)+'\n')
+        p = 0
+        q = 0
+        while p == q:
+            p = randprime(interval_boundaries[i], interval_boundaries[i+1])
+            q = randprime(interval_boundaries[i], interval_boundaries[i+1])
+        file.write(f"{p*q},{p},{q}\n")
