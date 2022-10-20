7237bb6d2dfda064494361d9322e173435c9d4e7
aritra <aritra@kth.se>
2022-10-20 23:39:59 +1100

    Filter benchmark

--- plot-benchmarks.py
+++ plot-benchmarks.py
@@ -5,17 +5,17 @@ from algorithms import ALGORITHMS
 plt.rc('text', usetex=True)
 
 bench = pd.read_csv('benchmark.csv')
+bench = bench[(bench['run_id']>=20)&(bench['time']!='Failed')]
 for alg in ALGORITHMS:
     x = bench[bench['algorithm']==alg]['N'].to_numpy()
     xint = []
     for el in x:
         xint += [int(el)]
-    y = bench[bench['algorithm']==alg]['time']
-    plt.semilogx(xint, y, marker='o', linestyle='None', label=alg)
+    y = pd.to_numeric(bench[bench['algorithm']==alg]['time'])
+    plt.loglog(xint, y, marker='o', label=alg)
 
 plt.xlabel('$N$')
 plt.ylabel('Time [s]')
 plt.legend()
-
 
 plt.show()
